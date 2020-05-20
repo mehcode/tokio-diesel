cc39caafca6d7bfe9d96be39ea78ac999e82399c
Joe Grund <jgrund@whamcloud.io>
2020-05-20 16:56:03 -0400

    Use task::block_in_place
    Use `task::block_in_place` with allows a number of `'static` lifetimes
    to be removed.
    
    This now works outside a task spawn due to
    https://github.com/tokio-rs/tokio/pull/2410 with the restriction that a
    threaded runtime is used.
    
    I've also added in github actions to ensure this change works (and
    continues to) :) .
    
    Fixes #9.
    
    Signed-off-by: Joe Grund <jgrund@whamcloud.io>

---
+++ tests/create_users.sql
@@ -0,0 +1,1 @@
+CREATE table users (id uuid);
---
+++ tests/integration_test.rs
@@ -0,0 +1,43 @@
+#[macro_use]
+extern crate diesel;
+
+use diesel::{
+    prelude::*,
+    r2d2::{ConnectionManager, Pool},
+    sql_query,
+};
+use std::error::Error;
+use tokio_diesel::*;
+use uuid::Uuid;
+
+// Schema
+table! {
+    users (id) {
+        id -> Uuid,
+    }
+}
+
+#[tokio::test(threaded_scheduler)]
+async fn test_db_ops() -> Result<(), Box<dyn Error>> {
+    let manager = ConnectionManager::<PgConnection>::new("postgres://postgres@localhost");
+    let pool = Pool::builder().build(manager)?;
+
+    let _ = sql_query(include_str!("./create_users.sql"))
+        .execute_async(&pool)
+        .await;
+
+    // Add
+    println!("add a user");
+    diesel::insert_into(users::table)
+        .values(users::id.eq(Uuid::new_v4()))
+        .execute_async(&pool)
+        .await?;
+
+    // Count
+    let num_users: i64 = users::table.count().get_result_async(&pool).await?;
+    println!("now there are {:?} users", num_users);
+
+    assert!(num_users > 0);
+
+    Ok(())
+}
